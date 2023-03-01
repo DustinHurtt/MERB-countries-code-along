d27c0cfb4957fe8af3900324f9360f813ee6f6e9
Dustin Hurtt <dustin_hurtt@outlook.com>
2023-03-01 14:59:22 -0500

    adding routes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,17 @@ import './App.css';
 
 import Navbar from './components/Navbar';
 
+import Countries from './pages/Countries';
+import EditPost from './pages/EditPost';
+import EditProfile from './pages/EditProfile';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import NewPost from './pages/NewPost';
+import OtherProfile from './pages/OtherProfile';
+import Posts from './pages/Posts';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+
 const App = () => {
 
   let token = localStorage.getItem("authToken");
@@ -21,15 +32,27 @@ const App = () => {
       <Navbar />
 
         <Routes>
-        
+
+
+          <Route path='/' element={<Home />} />
+          <Route path='/countries' element={<Countries />} />
+          <Route path='/posts' element={<Posts />} />
 
           <Route element={<LoggedIn />}>
-            
+
+            <Route path='/other-profile/:id' element={<OtherProfile />} />
+            <Route path='/post/:id' element={<EditPost />} />
+            <Route path='/edit-profile/:id' element={<EditProfile />} />
+            <Route path='/new-post' element={<NewPost />} />
+            <Route path='/profile/:id' element={<Profile />} />
+
           </Route>
 
-
           <Route element={<NotLoggedIn />}>
-            
+
+            <Route path='/signup' element={<Signup />} />
+            <Route path='/login' element={<Login />} />
+
           </Route>
 
 
